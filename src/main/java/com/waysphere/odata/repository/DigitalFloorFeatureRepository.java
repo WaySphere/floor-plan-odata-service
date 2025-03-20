b8515ca9cebe7657f86ee4b6f647ff7befa09289
bikramkumar080 <69755458+bikramkumar080@users.noreply.github.com>
2025-03-20 13:35:11 +0530

    Added put and delete call for floor features (#3)

--- src/main/java/com/waysphere/odata/repository/DigitalFloorFeatureRepository.java
+++ src/main/java/com/waysphere/odata/repository/DigitalFloorFeatureRepository.java
@@ -1,7 +1,9 @@
 package com.waysphere.odata.repository;
 
 import com.waysphere.odata.model.DigitalFloorFeature;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,4 +20,15 @@ public interface DigitalFloorFeatureRepository extends JpaRepository<DigitalFloo
             (SELECT id FROM floor_map WHERE org_id = :orgId AND id = :floorId)
     """, nativeQuery = true)
     List<Object[]> findFeaturesAsGeoJSON(@Param("orgId") Long orgId, @Param("floorId") String floorId);
+
+    @Modifying
+    @Transactional
+    @Query(value = """
+    DELETE FROM digital_floor_feature 
+    WHERE floor_map_id IN (
+        SELECT id FROM floor_map WHERE org_id = :orgId AND id = :floorId
+    )
+""", nativeQuery = true)
+    int deleteByFloorId(@Param("orgId") Long orgId, @Param("floorId") String floorId);
+
 }
